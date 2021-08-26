6d0a4e0ab45dfbbcea46d7d89f6fb7ae492c76eb
Ranveer Raj <ranveerraj248@gmail.com>
2021-08-26 16:12:27 +0530

    added login form

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let searchBtn = document.querySelector('#search-btn');
 let searchBar = document.querySelector('.search-bar-container');
+let formBtn = document.querySelector('#login-btn');
+let loginForm = document.querySelector('.login-form-container');
+let formClose = document.querySelector('#form-close');
+
 
 window.onscroll = () => {
     searchBtn.classList.remove('fa-times');
@@ -10,5 +14,15 @@ window.onscroll = () => {
 searchBtn.addEventListener('click', () =>{
     searchBtn.classList.toggle('fa-times');
     searchBar.classList.toggle('active');
+
+});
+
+formBtn.addEventListener('click', () =>{
+    loginForm.classList.add('active');
+
+});
+
+formClose.addEventListener('click', () =>{
+    loginForm.classList.remove('active');
 
 });
